4eaf6ffd71d0f98484daf2052660bc2642bdbff6
AlirezaNoor <alirezang7575@gmail.com>
2024-09-30 13:04:19 +0330

    fixed

--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2cb255364b86cbde95d6df3af8269ac04f0466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a007f3d3e5827928241059ff6c9daecbdfdb88f")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
